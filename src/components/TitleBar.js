d318e059b96ec7020519a3c2d7ad7faafdd3008c
Colin Tindle <ampersand@underscore.space>
2021-04-29 14:28:52 +0100

    update title

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react"
 
-const TitleBar = ({handleSelectChange, genres}) =>{
-  return (
-    <div>
-      <h1>Hit Parade</h1>
-      <select onChange={handleSelectChange}>
-        {genres.map(genre => {
-          return <option key={genre.name} value={genre.url}>{genre.name}</option>
-        })}
-      </select>
-    </div>
-  );
-};
+const TitleBar = ({ handleSelectChange, genres }) => {
+    return (
+        <div>
+            <h1>Codeclan Hit Parade</h1>
+            <select onChange={handleSelectChange}>
+                {genres.map((genre) => {
+                    return (
+                        <option key={genre.name} value={genre.url}>
+                            {genre.name}
+                        </option>
+                    )
+                })}
+            </select>
+        </div>
+    )
+}
 
-export default TitleBar;
+export default TitleBar
